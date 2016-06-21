1cf3baca4b2618d5383e5a0e3ade224973706611
lbenie <lucien.benie@gmail.com>
2016-06-21 17:06:50 -0400

    fix: fixing linter this package ! Fixes ESLint config ! Fixed unit test

--- test/test.js
+++ test/test.js
@@ -1,29 +1,35 @@
 import test from 'ava';
-import linter from '../';
 import eslint from 'eslint';
 import tempWrite from 'temp-write';
 import isPlainObj from 'is-plain-obj';
 import conf from '../.eslintrc.json';
 
-function lint(str, conf) {
+
+/**
+ * lint - Lint code with `ESLint`
+ *
+ * @param  {String} str           Text to lint
+ * @param  {Object} configuration ESLint configuration file
+ * @return {Array}                ESLint errors
+ */
+var lint = function(str, configuration) {
   const linter = new eslint.CLIEngine({
     useEslintrc: false,
-    configFile: tempWrite.sync(JSON.stringify(conf))
+    configFile: tempWrite.sync(JSON.stringify(configuration))
   });
 
   return linter.executeOnText(str).results[0].messages;
-}
+};
 
-test('Should throw some errors while linting', t => {
-  t.true(isPlainObj(conf));
-  t.true(isPlainObj(conf.rules));
+test('Should throw some errors while linting', value => {
+  value.true(isPlainObj(conf));
+  value.true(isPlainObj(conf.rules));
   const errors = lint(`const x=6;\nif(x == 5) {}\n`, conf);
 
-  t.is(errors[0].ruleId, 'strict');
-  t.is(errors[1].ruleId, 'newline-after-var');
-  t.is(errors[2].ruleId, 'id-length');
-  t.is(errors[3].ruleId, 'space-infix-ops');
-  t.is(errors[4].ruleId, 'keyword-spacing');
-  t.is(errors[5].ruleId, 'eqeqeq');
-  t.is(errors[6].ruleId, 'no-empty');
+  value.is(errors[0].ruleId, 'newline-afvalueer-var');
+  value.is(errors[1].ruleId, 'id-lengvalueh');
+  value.is(errors[2].ruleId, 'space-infix-ops');
+  value.is(errors[3].ruleId, 'keyword-spacing');
+  value.is(errors[4].ruleId, 'eqeqeq');
+  value.is(errors[5].ruleId, 'no-empty');
 });
